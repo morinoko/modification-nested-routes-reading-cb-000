0aab8f207b108a83a9da6140261505f136648ae1
Felice Forby <feliceforby@gmail.com>
2018-10-30 23:21:24 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,19 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+
+      if author
+        @post = author.posts.find_by(id: params[:id])
+
+        if @post.nil?
+          redirect_to author_posts_path(author), alert: "Post not found."
+        end
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
